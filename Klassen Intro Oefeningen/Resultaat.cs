93eba9551ae5f36c88b3b659b89354330c607160
ThomasDL129 <deleener_thomas@outlook.be>
2021-04-12 16:55:18 +0200

    Revert "Delete Klassen Intro Oefeningen directory"
    This reverts commit 8bf730502cbf67fbfdf9eef8bc7455e53d05b8a4.

---
+++ Klassen Intro Oefeningen/Resultaat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassen_Intro_Oefeningen
+{
+    class Resultaat
+    {
+        public int Percentage { get; set; } = 0;
+
+        public void PrintGraad()
+        {
+            if(Percentage < 50)
+            {
+                Console.WriteLine("Niet Geslaagd.");
+            }
+            else if(Percentage >= 50 && Percentage < 68)
+            {
+                Console.WriteLine("Voldoende.");
+            }
+            else if (Percentage >= 68 && Percentage < 75)
+            {
+                Console.WriteLine("Onderscheiding.");
+            }
+            else if (Percentage >= 75 && Percentage < 85)
+            {
+                Console.WriteLine("Grote onderscheiding.");
+            }
+            else
+            {
+                Console.WriteLine("Grootste onderscheiding.");
+            }
+        }
+    }
+}
