3ba1fc7155ac8eca0d6e22bd2a24803960162ca1
ThomasDL129 <deleener_thomas@outlook.be>
2021-04-12 15:32:38 +0200

    Add project files.

---
+++ Klassen Intro Oefeningen/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Klassen_Intro_Oefeningen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resultaat mijnpunten = new Resultaat();
+            mijnpunten.Percentage = 65;
+            mijnpunten.PrintGraad();
+        }
+    }
+}
