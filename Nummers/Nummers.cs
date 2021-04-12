6431dd77df89a006cd20282e52d84bc3fb612617
ThomasDL129 <deleener_thomas@outlook.be>
2021-04-12 16:55:14 +0200

    Revert "Delete Nummers directory"
    This reverts commit 8ee9826d952ec487bb13dfeb8f623e5daa74444e.

---
+++ Nummers/Nummers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nummers
+{
+    class Nummers
+    {
+        public int Getal1 { get; set; }
+        public int Getal2 { get; set; }
+
+        public int Som()
+        {
+            return Getal1 + Getal2;
+        }
+        public int Verschil()
+        {
+            return Getal1 - Getal2;
+        }
+        public int Product()
+        {
+            return Getal1 * Getal2;
+        }
+        public double Quotient()
+        {
+            return Convert.ToDouble(Getal1) / Getal2;
+        }
+    }
+}
